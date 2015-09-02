ed760d20f04d4179f6d3a6fddda4c4e363b3d8c4
Gianmaria Leoni <gianmaria.leoni@gmail.com>
2015-09-02 11:04:00 +0200

    Initila Commit

---
+++ www/js/controllers.js
@@ -0,0 +1,78 @@
+angular.module('starter.controllers', [])
+
+.service('TripsService', function($q) {
+  return {
+    trips: [
+      { title: 'Capodanno Amsterdam', id: 1, from: '31/12/2014', to: '03/01/2015', img: 'http://www.offerteshopping.it/wp-content/uploads/2015/04/amsterdam-lastminute.jpg' },
+      { title: 'Test', id: 2, from: '10/08/2014', to: '11/08/2014', img: 'http://www.offerteshopping.it/wp-content/uploads/2015/04/amsterdam-lastminute.jpg' },
+    ],
+    getTrips: function() {
+      return this.trips
+    },
+    getTrip: function(tripId) {
+      //var dfd = $q.defer()
+      var __trip;
+      this.trips.forEach(function(trip) {
+        if (trip.id === parseInt(tripId)) {
+          __trip = trip //dfd.resolve(trip)
+        }
+      })
+      console.log(__trip);
+      return __trip;
+      //return dfd.promise
+    }
+
+  }
+})
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  // Form data for the login modal
+  $scope.loginData = {};
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+})
+
+.controller('TripsCtrl', function($scope, trips) {
+  $scope.trips = trips;
+})
+
+.controller('TripCtrl', function($scope, $stateParams, TripsService){
+  //.controller('TripCtrl', function($scope, trip) {
+  //$scope.trip = trip;
+
+  $scope.trip = TripsService.getTrip($stateParams.tripId);
+});
