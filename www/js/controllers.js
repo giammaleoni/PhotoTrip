ae2bb6b2a7c110979375e352b3ef0e0d78a327b5
Gianmaria Leoni <gianmaria.leoni@gmail.com>
2015-09-07 18:07:47 +0200

    ng-disable e login con firebase

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,183 +5,198 @@ angular
 .service('TripsService', function($q) {
   return {
     trips: [{
-        title: 'Capodanno Amsterdam',
-        id: 0,
-        where: 'Amsterdam, Paesi Bassi',
-        from: '2014-12-31', to: '2015-01-03',
-        img: '/img/amsterdam.jpg',
-        friends: [
-          {id:0, admin:true},
-          {id:1, admin:false},
-        ],
-        album: [
-          {
-            id:0,
-            title: 'Tramonto',
-            descr:'tramonto sui canali',
-            by: {id: 1},
-            date:'2015-01-01',
-            img: '/img/amsterdam1.jpg',
-            geotag: { lat: 52.3717158, lng: 4.8916142 },
-            tags: ['tramonto', 'città']
-          },
-          {
-            id:1,
-            title: 'Test Title',
-            descr:'descrizione test',
-            by: {id: 1},
-            date:'2015-01-02',
-            img: '/img/amsterdam2.jpg',
-            geotag: { lat: 52.3749158, lng: 4.8986142 },
-            tags:['città'],
-          },
-        ],
-      },
-      {
-        title: 'Vacanze Romane',
-        id: 1,
-        from: '2014-08-10', to: '2014-08-11',
-        where: 'Roma, Italia',
-        img: '/img/roma.jpg',
-        friends:
-          [
-            {id:0, admin:true},
-            {id:1, admin:false},
-            {id:2, admin:false},
-          ],
-          album: [
-            {
-              id:0,
-              title: 'Tramonto',
-              descr:'tramonto sul tevere',
-              by: {id: 0},
-              date:'2014-08-10',
-              img: '/img/roma1.jpg',
-              geotag: { lat: 41.9100711, lng: 12.5369979 },
-              tags:['tramonto', 'città eterna'],
-            },
-            {
-              id:1,
-              title: 'Test',
-              descr:'descrizione test',
-              by: {id: 3},
-              date:'2014-08-11',
-              img: '/img/roma2.jpg',
-              geotag: { lat: 41.9127711, lng: 12.5351979 },
-              tags:[],
-            },
-            {
-              id:3,
-              title: 'Test',
-              descr:'descrizione test',
-              by: {id: 3},
-              date:'2014-08-11',
-              img: '/img/roma3.jpg',
-              geotag: { lat: 41.9119711, lng: 12.5325979 },
-              tags:[],
-            },
-          ],
-      },
-    ],
+      title: 'Capodanno Amsterdam',
+      id: 0,
+      where: 'Amsterdam, Paesi Bassi',
+      from: '2014-12-31', to: '2015-01-03',
+      img: '/img/amsterdam.jpg',
+      friends: [
+        {id:0, admin:true},
+        {id:1, admin:false},
+      ],
+      album: [
+        {
+          id:0,
+          title: 'Tramonto',
+          descr:'tramonto sui canali',
+          by: {id: 1},
+          date:'2015-01-01',
+          img: '/img/amsterdam1.jpg',
+          geotag: { lat: 52.3717158, lng: 4.8916142 },
+          tags: ['tramonto', 'città']
+        },
+        {
+          id:1,
+          title: 'Test Title',
+          descr:'descrizione test',
+          by: {id: 1},
+          date:'2015-01-02',
+          img: '/img/amsterdam2.jpg',
+          geotag: { lat: 52.3749158, lng: 4.8986142 },
+          tags:['città'],
+        },
+      ],
+    },
+    {
+      title: 'Vacanze Romane',
+      id: 1,
+      from: '2014-08-10', to: '2014-08-11',
+      where: 'Roma, Italia',
+      img: '/img/roma.jpg',
+      friends:
+      [
+        {id:0, admin:true},
+        {id:1, admin:false},
+        {id:2, admin:false},
+      ],
+      album: [
+        {
+          id:0,
+          title: 'Tramonto',
+          descr:'tramonto sul tevere',
+          by: {id: 0},
+          date:'2014-08-10',
+          img: '/img/roma1.jpg',
+          geotag: { lat: 41.9100711, lng: 12.5369979 },
+          tags:['tramonto', 'città eterna'],
+        },
+        {
+          id:1,
+          title: 'Test',
+          descr:'descrizione test',
+          by: {id: 3},
+          date:'2014-08-11',
+          img: '/img/roma2.jpg',
+          geotag: { lat: 41.9127711, lng: 12.5351979 },
+          tags:[],
+        },
+        {
+          id:3,
+          title: 'Test',
+          descr:'descrizione test',
+          by: {id: 3},
+          date:'2014-08-11',
+          img: '/img/roma3.jpg',
+          geotag: { lat: 41.9119711, lng: 12.5325979 },
+          tags:[],
+        },
+      ],
+    },
+  ],
 
-    friends: [
-      { id: 0, name: 'Claudia',   surname: 'Cassano', installed: true, other: "blablabla", img: '/img/anonimo.png'},
-      { id: 1, name: 'Gianmaria', surname: 'Leoni',   installed: true, other: "blablabla", img: '/img/anonimo.png'},
-      { id: 2, name: 'Alberto',   surname: 'Leoni',   installed: false, other: "blablabla", img: '/img/anonimo.png'},
-      { id: 3, name: 'Pinco',     surname: 'Pallino', installed: true, other: "blablabla", img: '/img/anonimo.png'},
-    ],
+  friends: [
+    { id: 0, name: 'Claudia',   surname: 'Cassano', installed: true, other: "blablabla", img: '/img/anonimo.png'},
+    { id: 1, name: 'Gianmaria', surname: 'Leoni',   installed: true, other: "blablabla", img: '/img/anonimo.png'},
+    { id: 2, name: 'Alberto',   surname: 'Leoni',   installed: false, other: "blablabla", img: '/img/anonimo.png'},
+    { id: 3, name: 'Pinco',     surname: 'Pallino', installed: true, other: "blablabla", img: '/img/anonimo.png'},
+  ],
 
 
-    getTrips: function() {
-      return this.trips
-    },
-    getTrip: function(tripId) {
-      //var dfd = $q.defer()
-      var __trip;
-      this.trips.forEach(function(trip) {
-        if (trip.id === parseInt(tripId)) {
-          __trip = trip //dfd.resolve(trip)
-        }
-      })
-      return __trip;
-      //return dfd.promise
-    },
-    getFriends: function() {
-      return this.friends
-    },
-    getTripFriends: function(tripId) {
-      var __trip;
-      this.trips.forEach(function(trip) {
-        if (trip.id === parseInt(tripId)) {
-          __trip = trip
-        }
-      })
-      return __trip.friends;
-    },
-    getFriendDetail: function(friend) {
-      friends = this.friends;
+  getTrips: function() {
+    return this.trips
+  },
+  getTrip: function(tripId) {
+    //var dfd = $q.defer()
+    var __trip;
+    this.trips.forEach(function(trip) {
+      if (trip.id === parseInt(tripId)) {
+        __trip = trip //dfd.resolve(trip)
+      }
+    })
+    return __trip;
+    //return dfd.promise
+  },
+  getFriends: function() {
+    return this.friends
+  },
+  getTripFriends: function(tripId) {
+    var __trip;
+    this.trips.forEach(function(trip) {
+      if (trip.id === parseInt(tripId)) {
+        __trip = trip
+      }
+    })
+    return __trip.friends;
+  },
+  getFriendDetail: function(friend) {
+    friends = this.friends;
+    for (var i = 0; i < friends.length; i++) {
+      if (friends[i].id === friend.id ) {
+        return friends[i];
+      }
+    }
+  },
+  getFriendsDetail: function(tripFriends) {
+    friends = this.friends;
+    tripFriends.forEach(function(friend, index, tf){
       for (var i = 0; i < friends.length; i++) {
         if (friends[i].id === friend.id ) {
-          return friends[i];
+          //friend = friends[i];
+          tf[index] = friends[i];
         }
       }
-    },
-    getFriendsDetail: function(tripFriends) {
-      friends = this.friends;
-      tripFriends.forEach(function(friend, index, tf){
-        for (var i = 0; i < friends.length; i++) {
-          if (friends[i].id === friend.id ) {
-            //friend = friends[i];
-            tf[index] = friends[i];
-          }
+    })
+    return tripFriends;
+  },
+  getTripFriendsDetail: function(trip, friends){
+    tripFriends = trip.friends
+    trip.friends = [];
+    friends.forEach(function(friend){
+      trip.friends.push(angular.copy(friend));
+      trip.friends[trip.friends.length - 1].admin = false;
+      for (var i = 0; i < tripFriends.length; i++) {
+        if (tripFriends[i].id === friend.id) {
+          trip.friends[i].admin = tripFriends[i].admin;
+          trip.friends[i].checked = true;
+          break;
+        }else{
+          //trip.friends[i].checked = false;
         }
-      })
-      return tripFriends;
-    },
-    getTripFriendsDetail: function(trip, friends){
-      tripFriends = trip.friends
-      trip.friends = [];
-      friends.forEach(function(friend){
-        trip.friends.push(angular.copy(friend));
-        trip.friends[trip.friends.length - 1].admin = false;
-        for (var i = 0; i < tripFriends.length; i++) {
-          if (tripFriends[i].id === friend.id) {
-            trip.friends[i].admin = tripFriends[i].admin;
-            trip.friends[i].checked = true;
-            break;
-          }else{
-            //trip.friends[i].checked = false;
-          }
-        }
-      });
-      trip.inited = true;
-      return trip
-    },
-    getPhotoDetail: function(album, friends){
-      album.forEach
+      }
+    });
+    trip.inited = true;
+    return trip
+  },
+  getPhotoDetail: function(album, friends){
+    album.forEach
 
-      album.inited = true;
-      return trip
-    },
-    localSaveTrip: function (trip){
-      //salva un trip nella local storage
-    },
-    localGetTrips: function(){
-      //riprende tutti i trip dalla local storage
-    },
-    localGetFriends: function(){
-      //riprende tutti gli amici dalla local storage
-    }
-
+    album.inited = true;
+    return trip
+  },
+  localSaveTrip: function (trip){
+    //salva un trip nella local storage
+  },
+  localGetTrips: function(){
+    //riprende tutti i trip dalla local storage
+  },
+  localGetFriends: function(){
+    //riprende tutti gli amici dalla local storage
   }
+
+}
 })
 
 .factory('InitService', function() {
-	var hasInited = false;
-	return hasInited;
+  var hasInited = false;
+  return hasInited;
 })
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, $ionicPopup) {
+
+  var ref = new Firebase('https://radiant-heat-1148.firebaseio.com');
+  // Form data for the login modal
+  $scope.loginData = {};
+
+  var authData = ref.getAuth();
+  if (authData) {
+    console.log("Authenticated user with uid:", authData.uid);
+    $scope.user = authData.password.email;
+    $scope.azione = "Log out";
+  }else{
+    console.log("No user logged");
+    $scope.user = "no user logged";
+    $scope.tipoAzione = "Log in";
+  }
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -190,9 +205,6 @@ angular
   //$scope.$on('$ionicView.enter', function(e) {
   //});
 
-  // Form data for the login modal
-  $scope.loginData = {};
-
   // Create the login modal that we will use later
   $ionicModal.fromTemplateUrl('templates/login.html', {
     scope: $scope
@@ -200,9 +212,21 @@ angular
     $scope.modal = modal;
   });
 
+  // Create the signup modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/signup.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal2 = modal;
+  });
+
   // Triggered in the login modal to close it
   $scope.closeLogin = function() {
     $scope.modal.hide();
+  };
+
+  // Triggered in the login modal to close it
+  $scope.closeSignup = function() {
+    $scope.modal2.hide();
   };
 
   // Open the login modal
@@ -210,16 +234,107 @@ angular
     $scope.modal.show();
   };
 
+  // Open the signup modal
+  $scope.signup = function() {
+    $scope.modal2.show();
+  };
+
+  $scope.showConfirm = function(){
+    var confirmPopup = $ionicPopup.confirm({
+      title: 'Validation Error',
+      template: 'Please enter username and password.',
+      okText: 'Try Again',
+      cancelText: 'Cancel'
+    });
+    confirmPopup.then(function(tryAgain) {
+      if (tryAgain) {
+        $scope.dologin();
+      }else {
+        $scope.closeLogin();
+      }
+    });
+  }
+
+
   // Perform the login action when the user submits the login form
   $scope.doLogin = function() {
     console.log('Doing login', $scope.loginData);
+    var email = $scope.loginData.username;
+    var password = $scope.loginData.password;
+    if(email && password){
+      ref.authWithPassword({
+        "email": email,
+        "password": password
+      }, function(error, authData) {
+        if (error) {
+          console.log("Login Failed!", error);
+        } else {
+          console.log("Authenticated successfully with payload:", authData);
+          $scope.user = authData.password.email;
+          $scope.closeLogin();
+        }
+      });
+    }
+    else{
+      $scope.showConfirm();
+    };
+  }
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
+  $scope.doLogout = function() {
+    ref.unauth();
+    $scope.user = "no user logged"
+  }
+
+  $scope.azione = function () {
+    authData = ref.getAuth();
+    if (authData) {
+      $scope.doLogout();
+    }else{
+      $scope.login();
+    }
+  }
+
+  $scope.doSignup = function() {
+    console.log('Doing signup', $scope.loginData);
+    var email = $scope.loginData.username;
+    var password = $scope.loginData.password;
+    if(email && password){
+      ref.createUser({
+        email: email,
+        password: password
+      }, function(error, userData) {
+        if (error) {
+          switch (error.code) {
+            case "EMAIL_TAKEN":
+            console.log("The new user account cannot be created because the email is already in use.");
+            break;
+            case "INVALID_EMAIL":
+            console.log("The specified email is not a valid email.");
+            break;
+            default:
+            console.log("Error creating user:", error);
+          }
+        } else {
+          console.log("Successfully created user account with uid:", userData.uid);
+          $scope.closeSignup();
+        }
+      });
+    }
+    else{
+      $scope.showConfirm();
+    };
+  }
+
+  //Create a callback which logs the current auth state
+  function authDataCallback(authData) {
+    if (authData) {
+      $scope.tipoAzione = "Log out";
+    } else {
+      $scope.tipoAzione = "Log in";
+    }
+  }
+  ref.onAuth(authDataCallback);
+
 })
 
 .controller('TripsCtrl', function($scope, trips) {
